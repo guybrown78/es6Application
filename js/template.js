11a514055cfddd1ade493133a56cdf3f5aa5e479
guybrown <guybrown@W7-GUY-12-11-13.DDLREDCAR.LOCAL>
2017-11-16 14:12:45 +0000

    improved tag html string and recurive templating

--- js/template.js
+++ js/template.js
@@ -1,28 +1,25 @@
 
-const html = (literal, ...cooked) => {
-  let result = '';
-  cooked.forEach((cook, i) => {
-    let lit = literal[i];
-    if (Array.isArray(cook)) {
-      cook = cook.join('');
-    }
-    result += lit;
-    result += cook;
-  });
-  result += literal[literal.length - 1];
-  return result;
+
+//const htmlString = (strings, ...values) => {
+function htmlString(strings, ...values){
+    let str = '';
+    values.forEach((val, i) => {
+        let string = strings[i];
+        if (Array.isArray(val))
+            val = val.join('');
+        //str += string + (val || '');
+        str += string;
+        str += val;
+    });
+    return str += strings[strings.length - 1];
 };
-const htmlExpr = (literal, ...expressions) => {
-  return literal.reduce((accumulator, part, i) => {
-    return accumulator + expressions[i - 1].toUpperCase() + part;
-  });
-};
-const node = data => html`
-  <li data-depth="${data.depth}">
-    <p class="node">${data.displayName}</p>
-  </li>
+const node = data => htmlString`
+    <li data-depth="${data.depth}">
+      <p class="node">${data.displayName}</p>
+    </li>
+    ${data.childrenCount ? treeTemplate(data) : ``}
 `;
-const treeTemplate = data => html`
+const treeTemplate = data => htmlString`
   <ul class="tree " data-children-count="${data.children.length}">
     ${data.children.map(data => node(data))}
   </ul>
--- js/view.js
+++ js/view.js
@@ -7,8 +7,9 @@ export default class View {
   };
   render(data) {
     this.el.innerHTML = treeTemplate(data);
-    for (const node of data.children){
+    /*for (const node of data.children){
+        console.log(node.childrenCount);
         console.table(node);
-    }
+    }*/
   };
 }
